4b37b681e0d9d2ebf905274a5acab2c49c90bb99
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-09 12:25:25 +0330

    Add navbar widget

---
+++ resources/widgets/navbar.php
@@ -0,0 +1,11 @@
+<?php include('../config/config.php'); ?>
+
+<nav class="navbar navbar-light bg-light fixed-top">
+    <div class="container-fluid">
+      <a href="<?php echo $path; ?>" class="navbar-brand">محصولات دانشگاه</a>
+      <div class="d-flex align-items-center">
+        <a href="<?php echo $path; ?>/y" class="text-reset me-3">اول</a>
+        <a href="x" class="text-reset me-3">دوم</a>
+      </div>
+    </div>
+</nav>
