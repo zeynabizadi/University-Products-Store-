2af60a833abc0a98f3ca16c00960ce51ff32d8b1
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-09 13:43:54 +0330

    Complete footer and navbar

--- pages/delivery.php
+++ pages/delivery.php
@@ -1,3 +1,9 @@
+<?php
+
+include('../resources/config/config.php');
+
+?>
+
 <!DOCTYPE html>
 <html lang="fa-IR">
 <head>
@@ -56,66 +62,7 @@
         -----------------START FOOTER---------------------
         --------------------------------------------------
        --------------------------------------------------->
-<footer>   
-<div class="container-fluid">
-  <footer class="py-5" style="text-align: center;">
-    <div class="row">
-      <div class="col-3">
-          <img src="resources/image/payment.png" alt="payment">
-          <h6>پرداخت امن از درگاه بانکی</h6>
-      </div> 
-      <div class="col-3">
-           <img src="resources/image/Quality.png" alt="Quality">
-           <h6>تضمین کیفیت کالا</h6>
-      </div> 
-      <div class="col-3">
-          <img  src="resources/image/Guarantee.png" alt="Guarantee">
-          <h6>فرصت 1 روزه بازگشت کالا</h6>
-      </div> 
-      <div class="col-3">
-        <img src="resources/image/Support.png" alt="Support">
-        <h6>پشتیبانی 24 ساعته</h6>
-      </div> 
-    </div>
-    <hr class="featurette-divider">
-    <br>
-    <div class="row">
-      <div class="col-2">
-        <h5>فهرست عناوین</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="back.php" class="nav-link p-0 text-muted">بازگشت کالا</a></li>
-          <li class="nav-item mb-2"><a href="warranty.php" class="nav-link p-0 text-muted"> ضمانت کالا</a></li>
-          <li class="nav-item mb-2"><a href="delivery.php" class="nav-link p-0 text-muted">نحوه تحویل کالا</a></li>
-        </ul>
-      </div>
-      <div class="col-2">
-        <h5>خدمات مشتریان</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="qustions.php" class="nav-link p-0 text-muted">سئوالات متداول</a></li>
-          <li class="nav-item mb-2"><a href="protest.php" class="nav-link p-0 text-muted">ثبت شکایت</a></li>
-          <li class="nav-item mb-2"><a href="guide.php" class="nav-link p-0 text-muted">راهنمای خرید</a></li>
-        </ul>
-      </div>
-      <div class="col-4 offset-1">
-        <form>
-          <p>با اشتراک در خبرنامه از آخرین و بهترین پیشنهادها مطلع شوید</p>
-          <div class="d-flex w-100 gap-2">
-            <button class="btn btn-dark" type="button">اشتراک</button>
-            <label for="newsletter1" class="visually-hidden">Email address</label>
-            <input id="newsletter1" type="text" class="form-control" placeholder="پست الکترونیکی خود را وارد کنید">
-          </div>
-        </form>
-      </div>
-      <div class="col-3">
-        <p>ارتباط با ما</p>
-        <ul class="share-buttons" data-source="simplesharingbuttons.com">
-          <li><a href="https://twitter.com/intent/tweet?source=&text=:%20" target="_blank" title="Tweet" onclick="window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(document.title) + ':%20'  + encodeURIComponent(document.URL)); return false;"><img alt="Tweet" src="resources/images/flat_web_icon_set/black/Twitter.png" /></a></li>
-          <li><a href="http://www.linkedin.com/shareArticle?mini=true&url=&title=&summary=&source=" target="_blank" title="Share on LinkedIn" onclick="window.open('http://www.linkedin.com/shareArticle?mini=true&url=' + encodeURIComponent(document.URL) + '&title=' +  encodeURIComponent(document.title)); return false;"><img alt="Share on LinkedIn" src="resources/images/flat_web_icon_set/black/LinkedIn.png" /></a></li>
-          <li><a href="mailto:?subject=&body=:%20" target="_blank" title="Send email" onclick="window.open('mailto:?subject=' + encodeURIComponent(document.title) + '&body=' +  encodeURIComponent(document.URL)); return false;"><img alt="Send email" src="resources/images/flat_web_icon_set/black/Email.png" /></a></li>
-        </ul>
-      </div>
-    </div>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+       <?php include('../resources/widgets/footer.php'); ?>
+  <script src="../resources/js/mdb.min.js"></script>
 </body>
 </html>
