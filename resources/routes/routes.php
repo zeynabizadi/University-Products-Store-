1c212d54b27e01fdba2251c03c98ad95b19a0f73
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-10 13:57:59 +0330

    Stuff

--- resources/routes/routes.php
+++ resources/routes/routes.php
@@ -1,4 +1,24 @@
 <?php
 
+session_start();
+
 include('../config/config.php');
 
+function logout($path) {
+    session_destroy();
+    header('location:', $path);
+}
+
+if (isset($_GET['logout'])) logout($path);
+
+if (isset($_POST['register'])) {
+    // Register
+}
+
+if (isset($_POST['login'])) {
+    // login
+}
+
+if (isset($_POST['newbook'])) {
+    // New book
+}
