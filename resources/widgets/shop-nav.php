198308abf73601efce4a98197357089312a35682
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-12 10:46:44 +0330

    Add stuff

---
+++ resources/widgets/shop-nav.php
@@ -0,0 +1,19 @@
+<?php echo null ?>
+
+<ul class="nav menu" style="border-top-left-radius: 0; border-top-right-radius: 0;">
+    <li class="nav-item">
+        <a class="nav-link" href="#anesthesia">علوم پایه</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#surgeryroom">علوم پزشکی</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#medical">علوم انسانی</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#nursing">فنی و مهندسی</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#nursing">هنر</a>
+    </li>
+</ul>
