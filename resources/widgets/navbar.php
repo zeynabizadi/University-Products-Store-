f9517baa663298c44551cf013a8a04b9da695625
zeynab <zeynabizadi788@yahoo.com>
2022-03-12 22:28:27 +0330

    Update navbar.php

--- resources/widgets/navbar.php
+++ resources/widgets/navbar.php
@@ -2,7 +2,7 @@
 
 <nav class="navbar navbar-light bg-light fixed-top">
     <div class="container-fluid">
-      <a href="<?php echo $path; ?>" class="navbar-brand">محصولات دانشگاه</a>
+      <a href="<?php echo $path; ?>" class="navbar-brand">محصولات دانشگاهی</a>
       <div class="d-flex align-items-center">
         <span class="text-reset me-3"><i class="fa fa-bars pointer" id="open-menu"></i></span>
       </div>
