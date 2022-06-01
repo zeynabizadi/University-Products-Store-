77ada8d861939a040938b178d664b42de8a8df6c
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-06-01 19:45:06 +0430

    Update pages dir resource path to cdn

--- pages/warranty.php
+++ pages/warranty.php
@@ -6,39 +6,42 @@ include('../resources/config/config.php');
 
 <!DOCTYPE html>
 <html lang="fa-IR">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="../resources/css/mdb.min.css" rel="stylesheet" type="text/css" rel="stylesheet">
-  <link rel="stylesheet" href="../resources/css/style.css">
+  <link href="https://blackiq.github.io/cdn-university-bookstore/css/mdb.min.css" rel="stylesheet" type="text/css" rel="stylesheet">
+  <link rel="stylesheet" href="https://blackiq.github.io/cdn-university-bookstore/css/style.css">
   <script src="https://kit.fontawesome.com/4a679d8ec0.js" crossorigin="anonymous"></script>
   <title>ضمانت کالا</title>
 </head>
+
 <body>
-<?php include('../resources/widgets/navbar.php'); ?>
-     <main>
-       <br>
-       <br>
-       <br>
-       <br>
-       <br>
-       <br>
-       <br>
-       <br>
-       <br>
-       <br>
-       <br>
-       <br>
-       <br>
-       <br>
-       <br>
-     </main>
-    <!-------------------------------------------------
+  <?php include('../resources/widgets/navbar.php'); ?>
+  <main>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+  </main>
+  <!-------------------------------------------------
         --------------------------------------------------
         -----------------START FOOTER---------------------
         --------------------------------------------------
        --------------------------------------------------->
-       <?php include('../resources/widgets/footer.php'); ?>
+  <?php include('../resources/widgets/footer.php'); ?>
   <script src="../resources/js/mdb.min.js"></script>
 </body>
+
 </html>
